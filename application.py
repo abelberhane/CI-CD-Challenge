cbe08d04658ebf6013ef4be8cf3f275b1933f0a4
Abel <abelberhane@gmail.com>
2021-09-05 13:43:31 -0400

    Added First true push

--- application.py
+++ application.py
@@ -1,1 +1,22 @@
-#Test 1
+# Setting up Flask and requests to build out the API
+from flask import Flask, requests
+app = Flask(__name__)
+
+
+# The initial landing page and home for my Application
+@app.route('/')
+def home():
+    return '<h1>Welcome to my first REST API.</h1><p>Please go to /hello to view a message.</p>'
+
+
+# Challenge Task 1.b - Implementing a GET request made to /hello that returns hello world
+@app.route('/hello')
+def hello_world():
+    headers = {'HELLO WORLD': 'HELLO WORLD'}
+    return 'hello world'
+
+#
+
+# This was scratched code that I was using to try to change the ports during a testing issue.
+#if __name__ == '__main__':
+#    app.run(host='127.0.0.1',port=7999, debug=True)
