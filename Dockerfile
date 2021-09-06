d2cfb786d20bc79146d3f8df547eea06c97e4714
Abel <abelberhane@gmail.com>
2021-09-06 08:15:21 -0400

    Updated code

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
-COPY src/.
+COPY . .
+
+EXPOSE 9001
 
 CMD ["python", "./application.py"]
--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 # Setting up Flask and requests to build out the API
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-import requests
 
 # The initial landing page and home for my Application
 @app.route('/')
@@ -11,17 +12,17 @@ def home():
 
 
 # Challenge Task 1.b - Implementing a GET request made to /hello that returns hello world
-@app.route('/hello', methods=['GET'])
+@app.route('/hello')
 def hello_world():
-    headers = {'HELLO WORLD': 'HELLO WORLD'}
+    # headers = {'HELLO WORLD': 'HELLO WORLD'}
     return 'hello world'
 
 
-
-
-
-
+# make sure I am listening to port 9001
 
 # This was scratched code that I was using to try to change the ports during a testing issue.
-#if __name__ == '__main__':
-#    app.run(host='127.0.0.1',port=7999, debug=True)
+if __name__ == "__main__":
+    print('This is running the port assignment side', flush=True)
+    app.run(host='0.0.0.0', port=9001)
+else:
+    print('I am being ran without port assignment', flush=True)
